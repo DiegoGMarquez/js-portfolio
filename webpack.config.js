dc9a405c4d1d3df9108a8d02bee9b8b658e4a456
Diego <marquezdiegogabriel@outlook.com>
2022-07-13 15:58:45 -0300

    Siguiendo curso Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-    },
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },  
     resolve: {
         extensions: ['.js']
     },
@@ -28,6 +31,26 @@ module.exports = {
                     'css-loader',
                     'stylus-loader'
                 ],
+            },
+            {
+                // Comprimir las imagenes a base 64, y usarlas como variables, loader de imagenes
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                // Fonts, optimizarlas y usar de forma local
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[contenthash].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    }
+                }
             }
         ]
     },
@@ -37,7 +60,10 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        // Mover elementos tanto archivos como carpetas
         new CopyPlugin({
             patterns: [
                 {
@@ -46,5 +72,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
